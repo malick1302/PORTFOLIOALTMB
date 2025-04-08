1a6ba3dc8e72d4985e0d598016cfa9a4d5571e70
Brice Bammez <bricebammez@MacBook-Pro-de-Brice.local>
2025-04-08 16:49:50 +0200

    Add Roter, Home, Navbar, Layout, indexCss

---
+++ my-portfolio2025/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './index.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Home from './pages/Home'
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+  
+  )
+}
+
+export default App
